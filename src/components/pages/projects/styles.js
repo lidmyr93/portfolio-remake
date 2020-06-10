ad7c0da8029fca23288cec5a1ec855bdd5166747
lidmyr93 <magnus.lidmyr@gloot.com>
2020-06-10 15:51:43 +0200

    Responsive masonry grid

--- src/components/pages/projects/styles.js
+++ src/components/pages/projects/styles.js
@@ -11,39 +11,61 @@ const HoverAnim = keyframes`
     }
 `;
 export const Item = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 1.3em;
-  font-weight: bold;
   color: white;
   background: ${(props) => `url(${props.background})`};
   background-position: top center;
   background-size: cover;
   background-repeat: no-repeat;
   height: 400px;
-  width: auto;
+  width: 90vw;
+  margin: 0 5px 10px 5px;
   filter: brightness(0.8);
   cursor: pointer;
   &:hover {
     animation ${HoverAnim} 1s forwards;
   }
+  @media screen and (min-width: 600px) {
+    height: 400px;
+    width: auto;
+    margin: 0;
+    background-size: cover;
+  }
 `;
 
 export const GridContainer = styled.div`
-  display: grid;
-  max-width: 1440px;
-  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-  grid-auto-flow: dense;
-  grid-column: 2 / 14;
-  grid-row: 2 / auto;
-  gap: 10px;
+  padding: 10px 0;
   ${ScrollBar}
-  .medium {
-    grid-column: span 1;
+  @media screen and (min-width: 600px) {
+    display: grid;
+    grid-auto-flow: dense;
+    gap: 10px;
+    grid-template-columns: 450px;
+    .big {
+      grid-column: span 2 / auto;
+    }
+    .medium {
+      grid-column: span 2 / auto;
+    }
   }
-
-  .big {
-    grid-column: span 2 / auto;
+  @media screen and (min-width: 750px) {
+    max-width: 600px;
+  }
+  @media screen and (min-width: 1000px) {
+    max-width: 800px;
+    grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+    .medium {
+      grid-column: span 1 / auto;
+    }
+    .big {
+      grid-column: span 2 / auto;
+    }
+  }
+  @media screen and (min-width: 1100px) {
+    max-width: 980px;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  }
+  @media screen and (min-width: 1350px) {
+    max-width: 1440px;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
   }
 `;
